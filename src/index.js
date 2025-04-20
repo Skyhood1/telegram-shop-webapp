2e8508d28ad4dec764e5c6d1738a94af7b7d687c
Skyhood1 <pavel.katkov.29011997@gmail.com>
2025-04-20 20:14:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-import App from './App';
+import './index.css';    // подключаем глобальные стили
+import App from './App';  // основной компонент
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
